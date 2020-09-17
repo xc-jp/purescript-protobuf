982b4561b418705ba40a06420f72f4cc84a49020
James Brock <jamesbrock@gmail.com>
2020-09-17 11:20:37 +0900

    conformance nix WIP

--- conformance.nix
+++ conformance.nix
@@ -20,36 +20,12 @@ pkgs.mkShell {
   ];
   shellHook = ''
     export PATH="./bin:$PATH"   # PATH to protoc-gen-purescript
+    set -e
     npm install
     spago build
     protoc --purescript_out=./conformance/generated --proto_path=${protobuf}/src --proto_path=${protobuf}/conformance ${protobuf}/src/google/protobuf/test_messages_proto3.proto"
-
-
-
-
-
-  export PATH="./bin:$PATH"   # PATH to protoc-gen-purescript
-  echo "Purescript Protobuf development environment."
-  echo "To build purescript-protobuf, run:"
-  echo ""
-  echo "    npm install"
-  echo "    spago build"
-  echo ""
-  echo "To test purescript-protobuf, run:"
-  echo ""
-  echo "    protoc --purescript_out=./test/generated test/*.proto"
-  echo "    spago -x test.dhall build"
-  echo "    spago -x test.dhall test"
-  echo ""
-  echo "To generate Purescript .purs files from .proto files, run:"
-  echo ""
-  echo "    protoc --purescript_out=path_to_output *.proto"
-  echo ""
-  echo "To run the Google conformance test on purescript-protobuf, run:"
-  echo ""
-  echo "    protoc --purescript_out=./generate-conformance --proto_path=conformance/result/src/  --proto_path=conformance/result/conformance/ ./conformance/result/src/google/protobuf/test_messages_proto3.proto"
-  echo "    spago -x conformance.dhall build"
-  echo "    spago -x conformance.dhall build"
+    spago -x conformance.dhall build
+    conformance-test-runner --enforce_recommended $(node -e "require('./output/Conformance.Main/index.js').main()")
   '';
   LC_ALL = "C.UTF-8"; # https://github.com/purescript/spago/issues/507
 }
