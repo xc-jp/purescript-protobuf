825ec87fa27eab6970be76b4a9b60f4204e6df98
James Brock <jamesbrock@gmail.com>
2020-11-12 00:59:09 +0900

    demo.nix from Github

--- nix/demo.nix
+++ nix/demo.nix
@@ -2,7 +2,12 @@
 # compiler plugin protoc-gen-purescript.
 { pkgs ? import ./pkgs.nix {} }:
 let
-  protoc-gen-purescript = import ../default.nix { inherit pkgs; };
+  purescript-protobuf = builtins.fetchGit {
+    url = "git@github.com:xc-jp/purescript-protobuf.git";
+    ref = "master";
+    rev = "23d2537c239450aa670c53edb491d46cd18c4422";
+  };
+  protoc-gen-purescript = import "${purescript-protobuf}/default.nix" {};
 in
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
@@ -10,5 +15,4 @@ pkgs.mkShell {
     protobuf3_9
     protoc-gen-purescript
   ];
-  LC_ALL = "C.UTF-8"; # https://github.com/purescript/spago/issues/507
 }
